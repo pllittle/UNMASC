42a03d36d3d1c759c558c4e6af6a58730d726a3b
pllittle <shadowsaver911@gmail.com>
2024-06-05 22:23:56 -0400

    up

--- workflow/tumor_only.sh
+++ workflow/tumor_only.sh
@@ -14,18 +14,10 @@ for repo in copythatdna somdna; do
 	check_array $repo copythatdna somdna UNMASC \
 		&& tmp_url=https://github.com/pllittle/$repo.git
 	
-	while true; do
-		if [ ! -d "$repo_dir" ]; then
-			cd "$git_dir"
-			git clone "$tmp_url" >&2
-			[ $? -eq 0 ] && break
-		# else
-			# cd "$repo_dir"
-			# git pull >&2
-			# [ $? -eq 0 ] && break
-		fi
-		echo -e "Some error in cloning $repo, contact pllittle" >&2 && return 1
-	done
+	if [ ! -d "$repo_dir" ]; then
+		cd "$git_dir"
+		git clone "$tmp_url" >&2
+	fi
 	
 done
 
